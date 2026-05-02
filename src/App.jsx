ccfa42873d0b1a7ec9bc7a80ac7c5a0b78482f25
Easha <eashabairy@gmail.com>
2026-05-03 03:40:12 +0530

    fixed skills jsx import

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "aos/dist/aos.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Skills from "./components/Skills";
+import Skills from "./components/Skills.jsx";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
